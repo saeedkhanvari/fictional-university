675c498671922aa0ef299b6329efa2e6db1afa0b
CallMeSaeed <86480508+saeedkhanvari@users.noreply.github.com>
2024-01-01 14:32:38 +0330

    test Commit

--- single.php
+++ single.php
@@ -1,10 +1,9 @@
-<?php get_header();
+<?php 
 
   while(have_posts()) {
     the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
     <h3><?php the_content() ?></h3>
-    <hr>
   <?php }
 
   // get_footer();
