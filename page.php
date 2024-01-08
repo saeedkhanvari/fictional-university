5406b5c6d49fd7ef1715b2936dac1506995388ea
CallMeSaeed <86480508+saeedkhanvari@users.noreply.github.com>
2024-01-08 16:14:52 +0330

    adding the get_the_permalink function

--- page.php
+++ page.php
@@ -4,24 +4,37 @@ get_header();
 while (have_posts()) {
     the_post(); ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
+        <div class="page-banner__bg-image"
+            style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title()?></h1>
+            <h1 class="page-banner__title">
+                <?php the_title() ?>
+            </h1>
             <div class="page-banner__intro">
                 <p>Learn how the school of your dreams got started.</p>
             </div>
         </div>
     </div>
 
-    <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-                    Us</a> <span class="metabox__main">Our History</span>
-            </p>
+    <?php
+    $theParentId = wp_get_post_parent_id(get_the_ID());
+    if ($theParentId) { ?>
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link"
+                        href="<?php echo get_the_permalink($theParentId) ?>"><i class="fa fa-home"
+                            aria-hidden="true"></i> Back to
+                        <?php echo get_the_title($theParentId) ?>
+                    </a> <span class="metabox__main">
+                        <?php echo the_title() ?>
+                    </span>
+                </p>
+            </div>
         </div>
+    <?php } ?>
 
-        <!-- <div class="page-links">
+    <!-- <div class="page-links">
             <h2 class="page-links__title"><a href="#">About Us</a></h2>
             <ul class="min-list">
                 <li class="current_page_item"><a href="#">Our History</a></li>
@@ -29,9 +42,9 @@ while (have_posts()) {
             </ul>
         </div> -->
 
-        <div class="generic-content">
-            <?php the_content()?>
-        </div>
+    <div class="generic-content">
+        <?php the_content() ?>
+    </div>
     </div>
 <?php }
 
