a7152d9c2b5c51e4d2d89a0c9a0666017ddbf824
CallMeSaeed <86480508+saeedkhanvari@users.noreply.github.com>
2024-02-06 18:30:37 +0330

    adding additional Links

--- front-page.php
+++ front-page.php
@@ -75,7 +75,7 @@ get_the_title(); // in this kind of function you will RETURN s.th that you can u
 
 
 
-      <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+      <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
     </div>
   </div>
 </div>
